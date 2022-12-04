e4f85914ab6678fe48b332486962507730b88502
venh <conf312@naver.com>
2022-12-04 14:07:11 +0900

    [add] prgm question #16

--- src/prgm/lv1/Practice_31.java
+++ src/prgm/lv1/Practice_31.java
@@ -1,6 +1,5 @@
 package prgm.lv1;
 
-import java.util.HashMap;
 import java.util.HashSet;
 
 /**
@@ -8,53 +7,28 @@ import java.util.HashSet;
  * [1차] 다트 게임
  */
 public class Practice_31 {
-
-
-
-    /*
-    1	1S2D*3T 	37	11 * 2 + 22 * 2 + 33
-    2	1D2S#10S	9	12 + 21 * (-1) + 101
-    3	1D2S0T	    3	12 + 21 + 03
-    4	1S*2T*3S	23	11 * 2 * 2 + 23 * 2 + 31
-    5	1D#2S*3S	5	12 * (-1) * 2 + 21 * 2 + 31
-    6	1T2D3D#	    -4	13 + 22 + 32 * (-1)
-    7	1D2S3T*	    59	12 + 21 * 2 + 33 * 2
-     */
     static int solution(String dartResult) {
-        int answer = 0;
         int index = 0;
-        int stageIndex = 0;
+        int sIndex = 0;
         String[] dartArr = dartResult.split("");
         String[] stages = new String[3];
-        HashMap<Integer, String> hashMap = new HashMap<>();
-        HashSet<String> hashSet = new HashSet<>();
-        for (int i=0; i<=10; i++) hashSet.add(String.valueOf(i));
 
-        // 1S2D*3T
+        HashSet<String> hashSet = new HashSet<>();
+
+        for (int i=0; i<=10; i++) hashSet.add(String.valueOf(i));
         for (int i=0; i<dartArr.length; i++) {
-            if (stageIndex == 2) {
-                stages[stageIndex] = dartResult.substring(index, dartArr.length);
+            if (sIndex == 2) {
+                stages[sIndex] = dartResult.substring(index, dartArr.length);
             } else if (i > 0 && hashSet.contains(dartArr[i])) {
-                stages[stageIndex] = dartResult.substring(index, i);
+                if ((dartArr[i-1] + dartArr[i]).equals("10")) continue;
+                stages[sIndex] = dartResult.substring(index, i);
                 index = i;
-                stageIndex++;
+                sIndex++;
             }
-//            else if (i+1 == dartArr.length) { // last
-//                System.out.println(stages[0]);
-//                System.out.println(stages[2]);
-//                System.out.println(stages[1]);
-//                stages[stageIndex] = dartResult.substring(index, dartArr.length);
-//            }
         }
 
-        System.out.println(stages[0]);
-        System.out.println(stages[1]);
-        System.out.println(stages[2]);
-        System.out.println("==========================");
         int[] result = stageReplace(stages);
-        System.out.println(result[0]);
-        System.out.println(result[1]);
-        System.out.println(result[2]);
+
         return result[0] + result[1] + result[2];
     }
 
@@ -62,56 +36,44 @@ public class Practice_31 {
         int[] result = new int[3];
 
         for (int i=0; i<stages.length; i++) {
-
-            int sdt =0, sqrt = 0;
+            int numberIndex = 0, sqrt = 0;
 
             if (stages[i].contains("S")) {
-                sdt = stages[i].indexOf("S");
+                numberIndex = stages[i].indexOf("S");
                 sqrt = 1;
             } else if (stages[i].contains("D")) {
-                sdt = stages[i].indexOf("D");
+                numberIndex = stages[i].indexOf("D");
                 sqrt = 2;
             } else {
-                sdt = stages[i].indexOf("T");
+                numberIndex = stages[i].indexOf("T");
                 sqrt = 3;
             }
 
-            int number = Integer.parseInt(stages[i].substring(0, sdt));
-
-            String[] stage = stages[i].split("");
+            int number = Integer.parseInt(stages[i].substring(0, numberIndex));
             int sum = (int) Math.pow(number, sqrt);
 
             if (stages[i].contains("*")) {
                 sum = sum * 2;
-
-                if (i > 0) {
-                    result[i-1] = result[i-1] * 2;
-                }
-
+                if (i > 0) result[i-1] = result[i-1] * 2;
             } else if (stages[i].contains("#")) {
                 sum = sum * (-1);
             }
 
             result[i] = sum;
         }
+
         return result;
     }
 
-
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
-        // 37
-        solution("1S2D*3T");
+        solution("10S10D*3T");
         //solution("1D2S#10S");
         //solution("1D2S0T");
         //solution("1S*2T*3S");
         //solution("1D#2S*3S");
-//        solution("1T2D3D#");
+        //solution("1T2D3D#");
         //solution("1D2S3T*");
-
-        //System.out.println("===> result: " + solution("1S2D*3T"));
-        // 9
-        //System.out.println("===> result: " + solution("1D2S#10S"));
         long end = System.currentTimeMillis();
         System.out.println("시간: " + (end - start) + " ms");
     }
--- src/prgm/lv1/Practice_32.java
+++ src/prgm/lv1/Practice_32.java
@@ -6,18 +6,55 @@ package prgm.lv1;
  */
 public class Practice_32 {
 
+    static void cardConv(int[][] arr, int index, int n) {
+        int i = 0;
+        while (n != 0) {
+            arr[index][i++] = n % 2;
+            n = n / 2;
+        }
+
+        int len = arr.length;
+        for (int y=0; y<len/2; y++) {
+            int tmp = arr[index][y];
+            arr[index][y] = arr[index][len-(y+1)];
+            arr[index][len-(y+1)] = tmp;
+        }
+    }
+
+
     static String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = {};
+        String[] answer = new String[n];
+
+        int[][] map1 = new int[n][n];
+        int[][] map2 = new int[n][n];
+
+        for (int i=0; i<n; i++) cardConv(map1, i, arr1[i]);
+        for (int j=0; j<n; j++) cardConv(map2, j, arr2[j]);
+
+        for (int i=0; i<n; i++) {
+            StringBuilder sBuilder = new StringBuilder();
+            for (int j=0; j<n; j++) {
+                sBuilder.append(String.valueOf(map1[i][j] | map2[i][j]));
+            }
+            answer[i] = sBuilder.toString();
+        }
+
+        for (int i=0; i<n; i++)
+            answer[i] = answer[i].replace("1","#").replace("0", " ");
+
         return answer;
     }
 
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
         int[] arr1 = {9, 20, 28, 18, 11};
         int[] arr2 = {30, 1, 21, 17, 28};
-        long start = System.currentTimeMillis();
+        int[] arr3 = {46, 33, 33 ,22, 31, 50};
+        int[] arr4 = {27 ,56, 19, 14, 14, 10};
 
         // ["#####","# # #", "### #", "# ##", "#####"]
-        solution(5, arr1, arr2);
+        //solution(5, arr1, arr2);
+        solution(6, arr3, arr4);
 
         long end = System.currentTimeMillis();
         System.out.println("시간: " + (end - start) + " ms");
