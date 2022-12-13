e77067527fc50faf6561e12e8cebef569a130047
venh <conf312@naver.com>
2022-12-13 22:13:46 +0900

    [add] prgm question #19

--- src/prgm/lv2/Practice_04.java
+++ src/prgm/lv2/Practice_04.java
@@ -1,5 +1,6 @@
 package prgm.lv2;
 
+import java.util.Arrays;
 
 /**
  * 2022 KAKAO BLIND RECRUITMENT
@@ -9,6 +10,43 @@ public class Practice_04 {
 
     static int[] solution(int n, int[] info) {
         int[] answer = new int[info.length];
+        int maxDiff = 0;
+        for (int i = 1; i < (1 << 10); i++) {
+            int[] tmp = new int[info.length];
+            int apeach = 0, ryan = 0, cnt = 0;
+            for (int j = 0; j < 10; j++) {
+                if ((i & (1 << j)) != 0) {
+                    tmp[j] = info[j] + 1;
+                    cnt += tmp[j];
+                    ryan += 10 - j;
+                } else {
+                    if (info[j] != 0)
+                        apeach += 10 - j;
+                }
+            }
+
+            if (cnt > n) continue;
+
+            tmp[10] = n - cnt;
+            int total = ryan - apeach;
+
+            if (total == maxDiff) {
+                for (int k = 9; k >= 0; k--) {
+                    if (tmp[k] > answer[k]) {
+                        maxDiff = total;
+                        answer = Arrays.copyOf(tmp, tmp.length);
+                        break;
+                    }
+                }
+            } else if (total > maxDiff) {
+                maxDiff = total;
+                answer = Arrays.copyOf(tmp, tmp.length);
+            }
+        }
+
+        if (maxDiff == 0)
+            return new int[]{-1};
+
         return answer;
     }
 
@@ -18,6 +56,12 @@ public class Practice_04 {
         int[] info3 = {0,0,1,2,0,1,1,1,1,1,1}; // 9, 1,1,2,0,1,2,2,0,0,0,0
         int[] info4 = {0,0,0,0,0,0,0,0,3,4,3}; // 10, 1,1,1,1,1,1,1,1,0,0,2
 
-        solution(5, info);
+        //solution(5, info);
+        for (int i : solution(5, info)) {
+            System.out.println(i);
+        }
+        //for (int i : solution(10, info4)) {
+            //System.out.println(i);
+        //}
     }
 }
---
+++ src/prgm/lv2/Practice_05.java
@@ -0,0 +1,13 @@
+package prgm.lv2;
+
+/**
+ */
+public class Practice_05 {
+
+    static int solution(int n, int[] info) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+    }
+}
