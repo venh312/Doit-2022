03095db16922c3f6d073bc9b7c880b7b9bf72559
venh <conf312@naver.com>
2022-12-16 22:08:56 +0900

    [add] prgm question #22

--- src/prgm/lv2/Practice_07.java
+++ src/prgm/lv2/Practice_07.java
@@ -5,16 +5,32 @@ package prgm.lv2;
  * 피로도
  */
 public class Practice_07 {
+    public static boolean check[];
+    public static int answer = 0;
+
+    static void dfs(int tired, int[][] dungeons, int cnt) {
+        for (int i = 0; i < dungeons.length; i++) {
+            if (!check[i] && dungeons[i][0] <= tired) {
+                check[i] = true;
+                dfs(tired-dungeons[i][1], dungeons, cnt+1);
+                check[i] = false;
+            }
+        }
+        answer = Math.max(answer, cnt);
+    }
+
 
     static int solution(int k, int[][] dungeons) {
-        int answer = -1;
+        check = new boolean[dungeons.length];
+
+        dfs(k, dungeons, 0);
 
         return answer;
     }
 
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
-        solution(80, new int[][] {{80,20}, {50,40}, {30,10}});
+        System.out.println("==> result: " + solution(80, new int[][] {{80,20}, {50,40}, {30,10}}));
         long end = System.currentTimeMillis();
         System.out.println("시간: " + (end - start) + " ms");
     }
